dac4a43504d538d90dc1226abef876920e8dd28b
devmustafa <developer.mustafa@gmail.com>
2016-11-20 22:29:39 +0400

    enhances

--- common/config/main.php
+++ common/config/main.php
@@ -7,8 +7,7 @@ return [
         ],
         'mongodb' => [
             'class' => '\yii\mongodb\Connection',
-            // 'dsn' => 'mongodb://192.168.5.150/vote', // local
-            'dsn' => 'mongodb://mongo:321@192.168.5.143/blog', // remote
+            'dsn' => 'mongodb://mongo:321@192.168.0.163/blog',
         ],
     ],
 ];
